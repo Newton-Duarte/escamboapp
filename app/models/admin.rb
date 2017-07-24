7c10c972269abc633ccea18f8ae1ac542ceb657e
Newton Duarte <newton_@hotmail.com>
2017-07-24 22:59:07 +0000

    Adicionado Scope lambda para admins

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,13 @@
 class Admin < ActiveRecord::Base
   enum role: [:full_access, :restricted_access]
 
+  scope :with_full_access, -> { where(role: 'full_access') }
+
+  # Faz a mesma coisa do scope acima
+  #def self.with_full_access
+  #  where(role: 'full_access')
+  #end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
